7ad6e9d2feab2188e80250d528dbfa1117ed4f70
leanderkretschmer <leanderkretschmer@icloud.com>
2025-11-05 12:53:50 +0100

    updated routes

--- init.rb
+++ init.rb
@@ -14,18 +14,3 @@ Redmine::Plugin.register :redmine_mailer_api do
   requires_redmine version_or_higher: '6.0.0'
 end
 
-# Routes hinzufügen - Routes werden direkt registriert
-Rails.application.config.to_prepare do
-  Rails.application.routes.draw do
-    # Route für E-Mail-Suche - verwende einen eindeutigen Pfad ohne user_id
-    # Dieser Pfad kollidiert nicht mit users/:user_id/mails
-    match 'mail_search(.:format)', :to => 'user_mails#search', :via => [:get], :as => 'search_user_mail'
-    
-    match 'users/:user_id/mails(.:format)', :to => 'user_mails#index', :via => [:get], :as => 'user_mails'
-    match 'users/:user_id/mails(.:format)', :to => 'user_mails#create', :via => [:post]
-    match 'users/:user_id/mails/:id(.:format)', :to => 'user_mails#show', :via => [:get], :as => 'user_mail'
-    match 'users/:user_id/mails/:id(.:format)', :to => 'user_mails#update', :via => [:put, :patch]
-    match 'users/:user_id/mails/:id(.:format)', :to => 'user_mails#destroy', :via => [:delete]
-  end
-end
-
